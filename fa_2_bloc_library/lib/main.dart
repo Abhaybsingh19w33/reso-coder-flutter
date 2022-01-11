3f64a382dc13264eca0309d50ab351dc24ff7710
Abhay Singh <abhaybsingh19w33@gmail.com>
2022-01-11 21:55:16 +0530

    Fix

--- fa_2_bloc_library/lib/main.dart
+++ fa_2_bloc_library/lib/main.dart
@@ -6,9 +6,11 @@ import 'package:fa_2_bloc_library/cubit/weather_cubit.dart';
 import 'package:fa_2_bloc_library/data/weather_repository.dart';
 import 'package:fa_2_bloc_library/pages/weather_search_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
